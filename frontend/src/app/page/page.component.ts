d0754075001163ea32f1f796c27ca25c5825f499
sggerard <sggerard1997@gmail.com>
2024-11-15 18:46:41 -0800

    Cleanup components

--- frontend/src/app/page/page.component.ts
+++ frontend/src/app/page/page.component.ts
@@ -1,6 +1,7 @@
 import { afterRender, Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Page } from '../../models/content/page';
+import { Page } from '../models/content/page';
+import { SafeHtmlPipe } from '@app/pipes/safe-html.pipe';
 
 @Component({
   selector: 'app-page',
